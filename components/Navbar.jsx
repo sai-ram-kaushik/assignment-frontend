c942a347e8871e5706dc782d34ff044fe625bc7c
sai-ram-kaushik <sairamkaushik1@gmail.com>
2023-07-13 23:17:20 +0530

    updated the navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,9 @@
 import Link from "next/link";
 import React, { useState } from "react";
 import { HiOutlineMenuAlt3 } from "react-icons/hi";
-import {AiOutlineClose} from 'react-icons/ai'
+import { AiOutlineClose } from "react-icons/ai";
+import ellipse from "../public/icons/1.png";
+import Image from "next/image";
 const Navbar = () => {
   const menuList = [
     {
@@ -28,9 +30,12 @@ const Navbar = () => {
   };
 
   return (
-    <div className="w-full h-20 shadow-md z-99">
+    <div className="w-full h-20 shadow-md z-50">
       <div className="flex items-center justify-between px-4 2xl:px-16 m-4">
-        <div className="flex items-center text-4xl font-orbitron font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#F86CF8] to-[#64CEF5]">
+        <div className="absolute -top-5 md:-top-10 md:left-[500px] -z-50">
+          <Image src={ellipse} alt="ellipse" width="" height=""/>
+        </div>
+        <div className="flex items-center text-3xl md:text-4xl lg:text-5xl font-orbitron font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#F86CF8] to-[#64CEF5]">
           ZURAVERSE
         </div>
         <div className="flex gap-5">
@@ -59,7 +64,7 @@ const Navbar = () => {
           <div className="flex items-center justify-center">
             <div className="flex w-full items-center justify-between ">
               <Link href="/">
-                <h3 className="flex items-center text-3xl font-orbitron font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#F86CF8] to-[#64CEF5]">
+                <h3 className="flex items-center text-xl font-orbitron font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#F86CF8] to-[#64CEF5]">
                   ZURAVERSE
                 </h3>
               </Link>
@@ -68,6 +73,16 @@ const Navbar = () => {
                 className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer text-black">
                 <AiOutlineClose />
               </div>
+            </div>
+          </div>
+
+          <div>
+            <div className="flex flex-col py-24 items-center gap-10 font-poppins text-black">
+              {menuList?.map((item, i) => (
+                <div key={i} className="text-xl hover:text-[#F86CF8]">
+                  <Link href="{item.link}">{item.name}</Link>
+                </div>
+              ))}
             </div>
           </div>
         </div>
